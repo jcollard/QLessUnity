4e18e3cbdad3a29d4097f43e45b42ed8fceb0430
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-10-08 16:30:59 -0700

    feat: Implement snap.

--- Endless QLess/Assets/Scripts/DieController.cs
+++ Endless QLess/Assets/Scripts/DieController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class DieController : MonoBehaviour
 {
     public TextMeshPro Label;
+    public SpriteRenderer Border;
     public char Face 
     {
         get => Label.text[0];
--- Endless QLess/Assets/Scripts/Draggable.cs
+++ Endless QLess/Assets/Scripts/Draggable.cs
@@ -8,6 +8,11 @@ public class Draggable : MonoBehaviour
     public Vector2 MousePosition => Camera.main.ScreenToWorldPoint(Input.mousePosition);
     public Vector2 Offset;
 
+    void Start()
+    {
+        // Snap();
+    }
+
     void OnMouseDown()
     {
         Offset = (Vector2)transform.position - MousePosition;
@@ -17,4 +22,15 @@ public class Draggable : MonoBehaviour
     {
         transform.position = MousePosition + Offset;
     }
+
+    public void Snap()
+    {
+        Vector3 position = transform.position;
+
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+        transform.position = position;
+    }
+
+    void OnMouseUp() => Snap();
 }
