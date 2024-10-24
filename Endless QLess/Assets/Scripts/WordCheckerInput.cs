c31ac5614df821b3ac394a7d701b6928696d8f3c
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-10-24 11:50:57 -0700

    feat: Add simple word checker.

---
+++ Endless QLess/Assets/Scripts/WordCheckerInput.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordCheckerInput : MonoBehaviour
+{
+    public Image Background;
+    public Color Valid = Color.green;
+    public Color Invalid = Color.red;
+    public TextAsset Dictionary;
+    private HashSet<string> _validWords;
+
+    void Awake()
+    {
+        _validWords = new HashSet<string>();
+        _validWords = Dictionary.text.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim().ToLower()).ToHashSet();
+        
+    }
+
+    public void Validate(string value)
+    {
+        string word = value.Trim().ToLower();
+        Color color = word switch {
+            "" => Color.white,
+            _ when _validWords.Contains(word) => Valid,
+            _ => Invalid
+
+        };
+        Background.color = color;
+    }
+}
